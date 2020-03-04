95e71fd064a2dda9381758ff087acb005c65f7b2
tatsuyafukui <nakedtatsuya@gmail.com>
2020-03-05 01:48:51 +0900

    lint

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,28 @@
 "use strict";
-const express = require("express");
-const app = express();
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const mysql_1 = __importDefault(require("mysql"));
+const connection = mysql_1.default.createConnection({
     host: '172.25.0.2',
     user: process.env.MYSQL_USER,
     database: process.env.MYSQL_DATABASE,
     password: process.env.MYSQL_ROOT_PASSWORD,
 });
 const PORT = process.env.PORT || 8080;
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     connection.query('SELECT * FROM users', function (err, results, fields) {
         console.log(results); // results contains rows returned by server'
-        let testdb = "not db";
+        let testdb = 'not db';
         if (results && results.length > 0) {
             testdb = results[0].name;
         }
         res.send(`
 							<h1>Docker + Node</h1>
-							<span>A match made in the cloud name is ${testdb}</span>
+							<span>Ass match made in the cloud name is ${testdb}</span>
 					`);
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,30 @@
-const express = require("express");
+import express from 'express';
 const app = express();
-const mysql = require('mysql2');
+import mysql from 'mysql';
 
 const connection = mysql.createConnection({
-		host: '172.25.0.2',
-		user: process.env.MYSQL_USER,
-		database: process.env.MYSQL_DATABASE,
-		password: process.env.MYSQL_ROOT_PASSWORD,
+  host: '172.25.0.2',
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_ROOT_PASSWORD,
 });
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req: any, res: any) => {
-	connection.query(
-			'SELECT * FROM users',
-			function(err: any, results: any, fields: any) {
-					console.log(results); // results contains rows returned by server'
-					let testdb = "not db";
-					if(results && results.length > 0) {
-							testdb = results[0].name
-					}
-					res.send(`
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM users', function(err, results, fields) {
+    console.log(results); // results contains rows returned by server'
+    let testdb = 'not db';
+    if (results && results.length > 0) {
+      testdb = results[0].name;
+    }
+    res.send(`
 							<h1>Docker + Node</h1>
-							<span>A match made in the cloud name is ${testdb}</span>
+							<span>Ass match made in the cloud name is ${testdb}</span>
 					`);
-			}
-	);
-
+  });
 });
 
 app.listen(PORT, () => {
-		console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
